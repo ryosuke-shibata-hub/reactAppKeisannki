d4b4b47b6ee5892677391c1effdffedafd2c8fb9
ryosuke-shibata-hub <ibis.stk@gmail.com>
2023-08-17 23:07:14 +0900

    計算機

---
+++ app/react-sample/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+// テキストボックスのコンポーネント
+class ValueInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.props.valueChange(e.target.value)
+    }
+    render() {
+        const newValue = this.props.value
+        return <input type='text' value={newValue} onChange={this.handleChange} />
+    }
+}
+
+// 計算の答えのコンポーネント
+class ValueAnswer extends React.Component {
+    render() {
+        return <span>{this.props.value}</span>
+    }
+}
+
+//計算用コンポーネント
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props)
+        this.changeValue1 = this.changeValue1.bind(this)
+        this.changeValue2 = this.changeValue2.bind(this)
+        this.state = {
+            value1: 0,
+            value2: 0,
+            answer: 0,
+        }
+    }
+    // 左のテキストボックス変更メソッド
+    changeValue1(newValue) {
+        let ans = parseInt(newValue) + parseInt(this.state.value2)
+        this.setState({
+            value1: newValue,
+            answer: ans,
+        })
+    }
+
+    // 右のテキストボックスメソッド
+    changeValue2(newValue) {
+        let ans = parseInt(newValue) + parseInt(this.state.value1)
+        this.setState({
+            value2: newValue,
+            answer: ans,
+        })
+    }
+    render() {
+        const newValue1 = this.state.value1
+        const newValue2 = this.state.value2
+        return (
+            <div>
+                <ValueInput value={newValue1} valueChange={this.changeValue1} /> +
+                <ValueInput value={newValue2} valueChange={this.changeValue2} /> =
+                <ValueAnswer value={this.state.answer} />
+            </div>
+        )
+    }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Calculator/>);
